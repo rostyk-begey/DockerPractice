500ebcd0469aab3040463d3f91fa624de63abeb5
Rostyslav Begey <rostyk.begey@gmail.com>
2019-06-27 01:41:36 +0300

    Installed Slim framework

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "
-<h1>Docker Practice LAMP 11111</h1>
-";
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$app = new Slim\App([
+    "settings" => [
+        "addContentLengthHeader" => false
+    ]
+]);
+
+$app->get('/', function (Request $request, Response $response) {
+    return $response->withJson([
+        "name" => "Manager",
+        "param" => $request->getQueryParam("param")
+    ]);
+});
+
+$app->run();
+
